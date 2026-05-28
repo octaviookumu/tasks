10a55da39ecc7d8bf847a67f24ae0c5b6153be47
octaviookumu <octaviookumu@gmail.com>
2026-05-28 17:30:18 +0300

    feat: update task list

--- backend/src/main/java/com/octaviookumu/tasks/controllers/TaskListController.java
+++ backend/src/main/java/com/octaviookumu/tasks/controllers/TaskListController.java
@@ -38,8 +38,22 @@ public class TaskListController {
     }
 
     @GetMapping("/{task_list_id}")
-    public Optional<TaskListDto> getTaskList(@PathVariable UUID taskListId) {
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId) {
         return taskListService.getTaskList(taskListId)
                 .map(taskListMapper::toDto);
     }
+
+    @PutMapping("/{task_list_id}")
+    public TaskListDto updateTaskList(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskListDto taskListDto) {
+
+        TaskList updatedTaskList = taskListService.updateTaskList(
+                taskListId, taskListMapper.fromDto(taskListDto)
+        );
+
+        return taskListMapper.toDto(updatedTaskList);
+
+    }
+
 }
